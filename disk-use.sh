05bdcc6c223498423f1bc003a9566221a1e89e21
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2024-10-08 00:46:53 +0530

    Disk-usage health check script added

---
+++ disk-use.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Change the threshold based on your needs
+THRESHOLD=10 # prefer to keep around 70% - 80%
+
+# Check disk usage and print a warning if usage is above the threshold
+df -H | grep -vE '^Filesystem|tmpfs|cdrom' | awk '{ print $5 " " $1 }' | while read output; do
+
+  usage=$(echo $output | awk '{ print $1}' | cut -d'%' -f1)
+  partition=$(echo $output | awk '{ print $2 }')
+
+  if [ $usage -ge $THRESHOLD ]; then
+    echo "Warning: Disk usage on $partition is at ${usage}%"
+  fi
+done
