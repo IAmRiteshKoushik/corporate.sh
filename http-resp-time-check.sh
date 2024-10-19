47ead704e512f0f0a188b54daa227a217276ce5b
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2024-10-19 23:41:24 +0530

    Add: HTTP resposne time checker script

---
+++ http-resp-time-check.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Add sample URLs
+URLS=("https://google.com/" "https://www.linkedin.com/")
+# Check HTTP response times for multiple URLs
+for URL in "${URLS[@]}"; do
+  RESPONSE_TIME=$(curl -o /dev/null -s -w '%{time_total}\n' $URL)
+  echo "Response time for $URL: $RESPONSE_TIME seconds"
+done
