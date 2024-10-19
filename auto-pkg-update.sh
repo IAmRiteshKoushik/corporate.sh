4e92a008c2f0460c812d914e1e7cf63c3119abee
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2024-10-19 23:32:24 +0530

    Fix: sudo added in command

--- auto-pkg-update.sh
+++ auto-pkg-update.sh
@@ -3,5 +3,5 @@
 # Update system packages and clean up unnecessary packages
 # "apt" is being used as all servers that I use are Ubuntu servers
 # Be careful with this as sometimes things break
-apt-get update && apt-get upgrade -y && apt-get autoremove -y && apt-get clean
+sudo apt-get update && apt-get upgrade -y && apt-get autoremove -y && apt-get clean
 echo "System packages are upgraded and cleaned up"
